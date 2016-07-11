cac5ac5c7645282e0f4dbec01abc3aa12395bb0a
Richard-Frost <frost.rchrd@gmail.com>
2016-07-11 11:44:33 -0700

    added a menu

--- lib/ny_baseball_headlines/cli.rb
+++ lib/ny_baseball_headlines/cli.rb
@@ -2,6 +2,7 @@ class NyBaseballHeadlines::CLI
 
   def call
     greeting
+    menu
   end
 
   def greeting
@@ -10,5 +11,31 @@ class NyBaseballHeadlines::CLI
     puts "***************************"
     puts ""
   end
+
+  def menu
+    puts "Press 1 for the latest Mets news"
+    puts "Press 2 for the latest Yankees news"
+    puts "Enter menu for options"
+
+    input = nil
+    while input != "exit"
+    input = gets.strip.downcase
+
+      if input.to_i == 1
+        puts ""
+        puts "NY Post"
+        puts "Mets Headlines"
+        puts "--------------"
+
+      elsif input.to_i == 2
+        puts ""
+        puts "NY Post"
+        puts "Yankees Headlines" 
+        puts "--------------"
+        elsif input == "menu"
+        menu
+      end
+    end
+  end 
 
 end

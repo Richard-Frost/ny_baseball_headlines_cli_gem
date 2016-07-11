1c2cb182bb56a46358130edbfde9c32029b6ce9c
Richard-Frost <frost.rchrd@gmail.com>
2016-07-11 16:19:25 -0700

    added news.rb to scrape website and fixed dependencies

--- lib/ny_baseball_headlines.rb
+++ lib/ny_baseball_headlines.rb
@@ -1,3 +1,7 @@
+require 'nokogiri'
+require 'pry'
+require 'open-uri'
 require "ny_baseball_headlines/version"
+require_relative "./ny_baseball_headlines/version"
 require_relative './ny_baseball_headlines/cli'
-
+require_relative './ny_baseball_headlines/news'
